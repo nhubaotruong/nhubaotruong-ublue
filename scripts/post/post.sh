22b6bd652fcfa83c7d31b2ee86afbbfb29471d31
nhubaotruong <nhubaotruong@users.noreply.github.com>
2023-05-13 22:48:00 +0700

    Fix font install conflicting compress option

--- scripts/post/post.sh
+++ scripts/post/post.sh
@@ -21,11 +21,11 @@ ln -sf /etc/fonts/conf.avail/99-ms-fonts.conf /etc/fonts/conf.d/
 
 # Microsoft Fonts
 curl -L http://archlinuxgr.tiven.org/archlinux/x86_64/ttf-ms-win11-auto-10.0.22621.525-1-any.pkg.tar.zst -o /tmp/ttf-ms-win11-auto.pkg.tar.zst
-tar --use-compress-program=unzstd -xzvf /tmp/ttf-ms-win11-auto.pkg.tar.zst -C /
+tar --use-compress-program=unzstd -xvf /tmp/ttf-ms-win11-auto.pkg.tar.zst -C /
 
 # JoyPixels
 curl -L https://archive.archlinux.org/packages/t/ttf-joypixels/ttf-joypixels-7.0.0-1-any.pkg.tar.zst -o /tmp/ttf-joypixels.pkg.tar.zst
-tar --use-compress-program=unzstd -xzvf /tmp/ttf-joypixels.pkg.tar.zst -C /
+tar --use-compress-program=unzstd -xvf /tmp/ttf-joypixels.pkg.tar.zst -C /
 
 # Nerd Fonts
 curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.0/Meslo.zip -o /tmp/Meslo.zip
