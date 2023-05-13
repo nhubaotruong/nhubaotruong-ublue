a671cc346f8fb99477e7d08b9cdd9c6f1c21180e
nhubaotruong <nhubaotruong@users.noreply.github.com>
2023-05-13 16:58:07 +0700

    Fix override

--- scripts/post/override.sh
+++ scripts/post/override.sh
@@ -2,4 +2,4 @@
 
 set -oeux pipefail
 
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:calcastor:gnome-patched mutter gnome-shell
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:calcastor:gnome-patched mutter
