9b70feedfb906e1baeae03d798f3887fea3d2efb
nhubaotruong <nhubaotruong@users.noreply.github.com>
2023-05-14 15:50:45 +0700

    Try teamviewer again

--- scripts/pre/pre.sh
+++ scripts/pre/pre.sh
@@ -2,5 +2,5 @@
 
 set -oeux pipefail
 
-# curl -L https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm -o /tmp/teamviewer.rpm
-curl -L "$(curl -L "https://www.expressvpn.com/latest#linux" | grep "Fedora 64-bit" | grep -m 1 -oP '(?<=value=")(.+)(?=")')" -o /tmp/expressvpn.rpm
+curl -Ls https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm -o /tmp/teamviewer.rpm
+curl -Ls "$(curl -L "https://www.expressvpn.com/latest#linux" | grep "Fedora 64-bit" | grep -m 1 -oP '(?<=value=")(.+)(?=")')" -o /tmp/expressvpn.rpm
