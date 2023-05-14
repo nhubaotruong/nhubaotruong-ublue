b6d9c2855a7de0eb5048c006a2743c35e1f3059a
nhubaotruong <nhubaotruong@users.noreply.github.com>
2023-05-14 16:35:11 +0700

    Let's install font on client and not in CI

--- scripts/post/post.sh
+++ scripts/post/post.sh
@@ -21,32 +21,32 @@ ln -sf /etc/fonts/conf.avail/75-joypixels.conf /etc/fonts/conf.d/
 ln -sf /etc/fonts/conf.avail/99-ms-fonts.conf /etc/fonts/conf.d/
 
 # Microsoft Fonts
-curl -Ls http://archlinuxgr.tiven.org/archlinux/x86_64/ttf-ms-win11-auto-10.0.22621.525-1-any.pkg.tar.zst -o /tmp/ttf-ms-win11-auto.pkg.tar.zst
-tar --use-compress-program=unzstd -xvf /tmp/ttf-ms-win11-auto.pkg.tar.zst -C /
-chown -R root: /usr/share/fonts/TTF
-chmod 644 /usr/share/fonts/TTF/*
-restorecon -vFr /usr/share/fonts/TTF
+# curl -Ls http://archlinuxgr.tiven.org/archlinux/x86_64/ttf-ms-win11-auto-10.0.22621.525-1-any.pkg.tar.zst -o /tmp/ttf-ms-win11-auto.pkg.tar.zst
+# tar --use-compress-program=unzstd -xvf /tmp/ttf-ms-win11-auto.pkg.tar.zst -C /
+# chown -R root: /usr/share/fonts/TTF
+# chmod 644 /usr/share/fonts/TTF/*
+# restorecon -vFr /usr/share/fonts/TTF
 
-# JoyPixels
-curl -Ls https://archive.archlinux.org/packages/t/ttf-joypixels/ttf-joypixels-7.0.0-1-any.pkg.tar.zst -o /tmp/ttf-joypixels.pkg.tar.zst
-tar --use-compress-program=unzstd -xvf /tmp/ttf-joypixels.pkg.tar.zst -C /
-chown -R root: /usr/share/fonts/joypixels
-chmod 644 /usr/share/fonts/joypixels/*
-restorecon -vFr /usr/share/fonts/joypixels
+# # JoyPixels
+# curl -Ls https://archive.archlinux.org/packages/t/ttf-joypixels/ttf-joypixels-7.0.0-1-any.pkg.tar.zst -o /tmp/ttf-joypixels.pkg.tar.zst
+# tar --use-compress-program=unzstd -xvf /tmp/ttf-joypixels.pkg.tar.zst -C /
+# chown -R root: /usr/share/fonts/joypixels
+# chmod 644 /usr/share/fonts/joypixels/*
+# restorecon -vFr /usr/share/fonts/joypixels
 
-# Nerd Fonts
-curl -Ls https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/Meslo.zip -o /tmp/Meslo.zip
-mkdir -p /usr/share/fonts/Meslo
-unzip /tmp/Meslo.zip -d /usr/share/fonts/Meslo
-chown -R root: /usr/share/fonts/Meslo
-chmod 644 /usr/share/fonts/Meslo/*
-restorecon -vFr /usr/share/fonts/Meslo
+# # Nerd Fonts
+# curl -Ls https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/Meslo.zip -o /tmp/Meslo.zip
+# mkdir -p /usr/share/fonts/Meslo
+# unzip /tmp/Meslo.zip -d /usr/share/fonts/Meslo
+# chown -R root: /usr/share/fonts/Meslo
+# chmod 644 /usr/share/fonts/Meslo/*
+# restorecon -vFr /usr/share/fonts/Meslo
 
-curl -Ls https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/FiraCode.zip -o /tmp/FiraCode.zip
-mkdir -p /usr/share/fonts/FiraCode
-unzip /tmp/FiraCode.zip -d /usr/share/fonts/FiraCode
-chown -R root: /usr/share/fonts/FiraCode
-chmod 644 /usr/share/fonts/FiraCode/*
-restorecon -vFr /usr/share/fonts/FiraCode
+# curl -Ls https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/FiraCode.zip -o /tmp/FiraCode.zip
+# mkdir -p /usr/share/fonts/FiraCode
+# unzip /tmp/FiraCode.zip -d /usr/share/fonts/FiraCode
+# chown -R root: /usr/share/fonts/FiraCode
+# chmod 644 /usr/share/fonts/FiraCode/*
+# restorecon -vFr /usr/share/fonts/FiraCode
 
 fc-cache -f
