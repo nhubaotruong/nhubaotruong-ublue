9eae1d302ffd136efc5d9c6a201152978ff75eb8
nhubaotruong <nhubaotruong@users.noreply.github.com>
2023-05-16 10:54:14 +0700

    Adw-gtk already in base image

--- scripts/post/post.sh
+++ scripts/post/post.sh
@@ -21,14 +21,14 @@ ln -sf /etc/fonts/conf.avail/75-joypixels.conf /etc/fonts/conf.d/
 ln -sf /etc/fonts/conf.avail/99-ms-fonts.conf /etc/fonts/conf.d/
 
 # Microsoft Fonts
-curl -Ls http://archlinuxgr.tiven.org/archlinux/x86_64/ttf-ms-win11-auto-10.0.22621.525-1-any.pkg.tar.zst -o /tmp/ttf-ms-win11-auto.pkg.tar.zst
+curl -L http://archlinuxgr.tiven.org/archlinux/x86_64/ttf-ms-win11-auto-10.0.22621.525-1-any.pkg.tar.zst -o /tmp/ttf-ms-win11-auto.pkg.tar.zst
 tar --use-compress-program=unzstd -xvf /tmp/ttf-ms-win11-auto.pkg.tar.zst -C /
 chown -R root: /usr/share/fonts/TTF
 chmod 644 /usr/share/fonts/TTF/*
 restorecon -vFr /usr/share/fonts/TTF
 
 # JoyPixels
-curl -Ls https://archive.archlinux.org/packages/t/ttf-joypixels/ttf-joypixels-7.0.0-1-any.pkg.tar.zst -o /tmp/ttf-joypixels.pkg.tar.zst
+curl -L https://archive.archlinux.org/packages/t/ttf-joypixels/ttf-joypixels-7.0.0-1-any.pkg.tar.zst -o /tmp/ttf-joypixels.pkg.tar.zst
 tar --use-compress-program=unzstd -xvf /tmp/ttf-joypixels.pkg.tar.zst -C /
 chown -R root: /usr/share/fonts/joypixels
 chmod 644 /usr/share/fonts/joypixels/*
@@ -40,10 +40,10 @@ nerd_fonts=(
     "Meslo"
     "FiraCode"
 )
-nerd_fonts_version="$(curl -Ls https://github.com/ryanoasis/nerd-fonts/releases/latest | grep '<h1' | grep -m 1 -oP '(?<=>)(.+)(?=</h1>)')"
+nerd_fonts_version="$(curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest | grep '<h1' | grep -m 1 -oP '(?<=>)(.+)(?=</h1>)')"
 
 for font in "${nerd_fonts[@]}"; do
-    curl -Ls "https://github.com/ryanoasis/nerd-fonts/releases/download/$nerd_fonts_version/$font.tar.xz" -o "/tmp/$font.tar.xz"
+    curl -L "https://github.com/ryanoasis/nerd-fonts/releases/download/$nerd_fonts_version/$font.tar.xz" -o "/tmp/$font.tar.xz"
     tar -xvf "/tmp/$font.tar.xz" -C /usr/share/fonts/ --one-top-level
     chown -R root: "/usr/share/fonts/$font"
     chmod 644 "/usr/share/fonts/$font"/*
