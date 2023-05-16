24f3dbcbaa99ab27470c329a2c5de76e29bc8e87
nhubaotruong <nhubaotruong@users.noreply.github.com>
2023-05-16 10:40:43 +0700

    Revert "Add nohang"
    This reverts commit 7eb9ff5aec2671c8f2616b489eb400a8cc837d97.

--- scripts/post/post.sh
+++ scripts/post/post.sh
@@ -5,16 +5,14 @@ set -oeux pipefail
 rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:calcastor:gnome-patched mutter
 
 systemctl disable \
-    systemd-timesyncd \
-    systemd-oomd
+    systemd-timesyncd
 
 systemctl enable \
     docker.socket \
     tlp \
     supergfxd \
     chronyd \
-    tailscaled \
-    nohang-desktop
+    tailscaled
 
 systemctl mask nvidia-powerd
 
@@ -23,14 +21,14 @@ ln -sf /etc/fonts/conf.avail/75-joypixels.conf /etc/fonts/conf.d/
 ln -sf /etc/fonts/conf.avail/99-ms-fonts.conf /etc/fonts/conf.d/
 
 # Microsoft Fonts
-curl -L http://archlinuxgr.tiven.org/archlinux/x86_64/ttf-ms-win11-auto-10.0.22621.525-1-any.pkg.tar.zst -o /tmp/ttf-ms-win11-auto.pkg.tar.zst
+curl -Ls http://archlinuxgr.tiven.org/archlinux/x86_64/ttf-ms-win11-auto-10.0.22621.525-1-any.pkg.tar.zst -o /tmp/ttf-ms-win11-auto.pkg.tar.zst
 tar --use-compress-program=unzstd -xvf /tmp/ttf-ms-win11-auto.pkg.tar.zst -C /
 chown -R root: /usr/share/fonts/TTF
 chmod 644 /usr/share/fonts/TTF/*
 restorecon -vFr /usr/share/fonts/TTF
 
 # JoyPixels
-curl -L https://archive.archlinux.org/packages/t/ttf-joypixels/ttf-joypixels-7.0.0-1-any.pkg.tar.zst -o /tmp/ttf-joypixels.pkg.tar.zst
+curl -Ls https://archive.archlinux.org/packages/t/ttf-joypixels/ttf-joypixels-7.0.0-1-any.pkg.tar.zst -o /tmp/ttf-joypixels.pkg.tar.zst
 tar --use-compress-program=unzstd -xvf /tmp/ttf-joypixels.pkg.tar.zst -C /
 chown -R root: /usr/share/fonts/joypixels
 chmod 644 /usr/share/fonts/joypixels/*
@@ -42,10 +40,10 @@ nerd_fonts=(
     "Meslo"
     "FiraCode"
 )
-nerd_fonts_version="$(curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest | grep '<h1' | grep -m 1 -oP '(?<=>)(.+)(?=</h1>)')"
+nerd_fonts_version="$(curl -Ls https://github.com/ryanoasis/nerd-fonts/releases/latest | grep '<h1' | grep -m 1 -oP '(?<=>)(.+)(?=</h1>)')"
 
 for font in "${nerd_fonts[@]}"; do
-    curl -L "https://github.com/ryanoasis/nerd-fonts/releases/download/$nerd_fonts_version/$font.tar.xz" -o "/tmp/$font.tar.xz"
+    curl -Ls "https://github.com/ryanoasis/nerd-fonts/releases/download/$nerd_fonts_version/$font.tar.xz" -o "/tmp/$font.tar.xz"
     tar -xvf "/tmp/$font.tar.xz" -C /usr/share/fonts/ --one-top-level
     chown -R root: "/usr/share/fonts/$font"
     chmod 644 "/usr/share/fonts/$font"/*
