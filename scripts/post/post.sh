8e0503096ad6253796af3a65da0746e1f2cb3b6f
nhubaotruong <nhubaotruong@users.noreply.github.com>
2023-05-27 11:10:10 +0700

    Fix awscli

--- scripts/post/post.sh
+++ scripts/post/post.sh
@@ -54,7 +54,7 @@ fc-cache -f
 
 # awscli
 curl -L "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
-unzip /tmp/awscliv2.zip
+unzip /tmp/awscliv2.zip -d /tmp
 /tmp/aws/install -i /usr/share/aws-cli -b /usr/bin
 
 # kubectl
