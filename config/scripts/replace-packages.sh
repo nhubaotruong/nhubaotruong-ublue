a893d5d1a4a5fcb082f94188e35ff80c09b2e5ee
nhubao <baoprokute1234@gmail.com>
2023-09-30 01:42:24 +0700

    Mutter patched

---
+++ config/scripts/replace-packages.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched gnome-shell mutter mutter-common xorg-x11-server-Xwayland
