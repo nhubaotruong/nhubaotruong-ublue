e731e5199547e618ce6496f7c383550123c516ed
nhubao <baoprokute1234@gmail.com>
2023-09-30 00:00:27 +0700

    Update nerd-fonts

--- config/scripts/nerd-fonts.sh
+++ config/scripts/nerd-fonts.sh
@@ -9,10 +9,11 @@ font_names=(
     "CascadiaCode"
 )
 
-font_version="$(curl -Ls https://github.com/ryanoasis/nerd-fonts/releases/latest | grep -m 1 -oP '(?<=<h1 data-view-component="true" class="d-inline mr-3">)(.+)(?=</h1>)')"
+RELEASE=$(curl -Ls "https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest")
 
 for font in "${font_names[@]}"; do
-    curl -L "https://github.com/ryanoasis/nerd-fonts/releases/download/$font_version/$font.tar.xz" -o "/tmp/$font.tar.xz"
+    url=$(echo "$RELEASE" | yq -r --unbuffered ".assets[] | select(.name == \"$font.tar.xz\") | .browser_download_url")
+    curl -L "$url" -o "/tmp/$font.tar.xz"
     tar -xvf "/tmp/$font.tar.xz" -C /usr/share/fonts/ --one-top-level
     chown -R root: "/usr/share/fonts/$font"
     chmod 644 "/usr/share/fonts/$font"/*
