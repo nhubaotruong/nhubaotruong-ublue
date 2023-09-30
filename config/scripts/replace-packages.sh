d45b9e5cae9ef297d9bb07e57f8a2062bf59e337
nhubao <baoprokute1234@gmail.com>
2023-09-30 18:09:08 +0700

    Mutter only

--- config/scripts/replace-packages.sh
+++ config/scripts/replace-packages.sh
@@ -3,4 +3,4 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched gnome-shell mutter mutter-common
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched mutter mutter-common
