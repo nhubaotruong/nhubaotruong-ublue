28be91aa2f96015469f46193158d7d593523c6de
nhubao <baoprokute1234@gmail.com>
2023-09-30 22:56:25 +0700

    Back to 38

--- config/scripts/replace-packages.sh
+++ config/scripts/replace-packages.sh
@@ -3,4 +3,4 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched mutter mutter-common
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched mutter mutter-common xorg-x11-server-Xwayland
