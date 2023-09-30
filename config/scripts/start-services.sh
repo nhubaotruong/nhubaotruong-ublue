611ef4772f0b56c90bc30982b9337d97f816e7fd
nhubao <baoprokute1234@gmail.com>
2023-09-30 21:43:12 +0700

    Add dnsproxy

--- config/scripts/start-services.sh
+++ config/scripts/start-services.sh
@@ -3,4 +3,4 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-systemctl enable tlp.service supergfxd.service tailscaled.service
+systemctl enable tlp.service supergfxd.service tailscaled.service dnsproxy.service
