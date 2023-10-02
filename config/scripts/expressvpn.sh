7dd6568fbb28051b2d749f919a2d158dec74a0b4
nhubao <baoprokute1234@gmail.com>
2023-10-02 12:37:18 +0700

    Receive gpg key of expressvpn

--- config/scripts/expressvpn.sh
+++ config/scripts/expressvpn.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-download_url=$(curl -L https://www.expressvpn.com/vn/latest\#linux | grep 'Fedora 64' | grep -m 1 -oP '(?<=value=")(.+)(?=")')
+RELEASE=$(curl -L https://www.expressvpn.com/vn/latest\#linux | grep 'Fedora 64')
+
+download_url=$(echo "$RELEASE" | grep -m 1 -oP '(?<=value=")(.+)(?=")')
+
+gpg_url=$(echo "$RELEASE" | grep -m 1 -oP '(?<=data-signature-uri=")(.+.asc)(?=")')
 
 curl -L "$download_url" -o /tmp/expressvpn.rpm
+
+curl -L "$gpg_url" -o /tmp/expressvpn.rpm.asc
+
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 0xAFF2A1415F6A3A38
+
+gpg --verify /tmp/expressvpn.rpm.asc
 
 rpm-ostree install /tmp/expressvpn.rpm
