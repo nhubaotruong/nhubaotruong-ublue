1697a8e09c6b915a80ab994812ec48fc7a67c61f
nhubao <baoprokute1234@gmail.com>
2023-10-02 12:11:16 +0700

    Add expressvpn and teamviewer

---
+++ config/scripts/expressvpn.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+download_url=$(curl -L https://www.expressvpn.com/vn/latest\#linux | grep 'Fedora 64' | grep -m 1 -oP '(?<=value=")(.+)(?=")')
+
+curl -L -o /tmp/expressvpn.rpm "$download_url"
+
+sudo rpm-ostree install /tmp/expressvpn.rpm
