5450a6862c8dc538df2039cc11a6a93060f1c19b
nhubao <baoprokute1234@gmail.com>
2023-10-02 23:55:57 +0700

    Install teamviewer post

--- config/scripts/teamviewer.sh
+++ config/scripts/teamviewer.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-curl -L https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm -o /tmp/teamviewer.rpm
+curl -L https://download.teamviewer.com/download/linux/teamviewer_amd64.tar.xz -o /tmp/teamviewer.rpm
 
-rpm-ostree install /tmp/teamviewer.rpm
+tar -xvf /tmp/teamviewer.rpm -C /tmp
+
+sh /tmp/teamviewer/tv-setup install force
