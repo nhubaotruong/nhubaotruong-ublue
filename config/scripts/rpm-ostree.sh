713bee985e37a83b081951d007b2ac3281dae0c6
nhubao <baoprokute1234@gmail.com>
2023-10-02 17:04:23 +0700

    Replace own bash script with modules upstream

--- config/scripts/rpm-ostree.sh
+++ config/scripts/rpm-ostree.sh
@@ -3,4 +3,5 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
+rm /etc/yum.repos.d/expressvpn.repo
 rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched mutter mutter-common xorg-x11-server-Xwayland
