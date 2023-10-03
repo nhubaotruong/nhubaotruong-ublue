2798520cbdb3ee9cb4d2afcb82093bf67ede8178
nhubao <baoprokute1234@gmail.com>
2023-10-03 13:02:04 +0700

    manual install teamviewer from rpm

--- config/scripts/teamviewer.sh
+++ config/scripts/teamviewer.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-curl -L https://download.teamviewer.com/download/linux/teamviewer_amd64.tar.xz -o /tmp/teamviewer.rpm
+curl -L https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm -o /tmp/teamviewer.rpm
 
-tar -xvf /tmp/teamviewer.rpm -C /usr/lib/
+mkdir -p /tmp/teamviewer
 
-ln -sf /usr/lib/teamviewer/tv_bin/script/teamviewer /usr/bin/teamviewer
+rpm2cpio /tmp/teamviewer.rpm | cpio -idmv -D /tmp/teamviewer
+
+mv /tmp/teamviewer/opt/teamviewer /usr/lib
+
+dir="/tmp/teamviewer/usr" # replace with your directory path
+old_path="/opt/teamviewer"
+new_path="/usr/lib/teamviewer"
+
+find "$dir" -type l | while read -r symlink; do
+    target=$(readlink "$symlink")
+    if [[ $target == $old_path* ]]; then
+        new_target="${target/$old_path/$new_path}"
+        ln -snf "$new_target" "$symlink"
+        echo "Fixed $symlink"
+    fi
+done
+
+cp -r /tmp/teamviewer/usr/* /usr/
