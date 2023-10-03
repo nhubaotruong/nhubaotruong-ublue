0513a2bd2f3d51ea716663eecef29a9a54b5a1b0
nhubao <baoprokute1234@gmail.com>
2023-10-03 15:20:22 +0700

    teamviewer

--- config/scripts/teamviewer.sh
+++ config/scripts/teamviewer.sh
@@ -3,15 +3,16 @@ set -oue pipefail
 
 curl -L https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm -o /tmp/teamviewer.rpm
 
-mkdir -p /tmp/teamviewer
+extracted_dir="/tmp/teamviewer"
 
-rpm2cpio /tmp/teamviewer.rpm | cpio -idmv -D /tmp/teamviewer
+mkdir -p $extracted_dir
 
-dir="/tmp/teamviewer"
+rpm2cpio /tmp/teamviewer.rpm | cpio -idmv -D $extracted_dir
+
 old_path="/opt/teamviewer"
 new_path="/usr/lib/teamviewer"
 
-find "$dir" -type l | while read -r symlink; do
+find "$extracted_dir" -type l | while read -r symlink; do
     target=$(readlink "$symlink")
     if [[ $target == $old_path* ]]; then
         new_target="${target/$old_path/$new_path}"
@@ -21,8 +22,8 @@ find "$dir" -type l | while read -r symlink; do
 done
 
 # find .desktop and .service files and fix them
-find "$dir" -type f -name "*.desktop" -o -name "*.service" | while read -r file; do
-    sed -i 's/\/opt/\/usr\/lib/g' "$file"
+find "$extracted_dir/opt/teamviewer" -type f -name "*.desktop" -o -name "*.service" | while read -r file; do
+    sed -i 's/\/opt\/teamviewer/\/usr\/lib\/teamviewer/g' "$file"
 done
 
 mv /tmp/teamviewer/opt/teamviewer /usr/lib
