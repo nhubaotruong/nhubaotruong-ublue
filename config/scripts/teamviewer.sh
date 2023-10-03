524750810031694cd139d1094349b0c1fc302645
nhubao <baoprokute1234@gmail.com>
2023-10-03 11:26:30 +0700

    Add teamviewer

--- config/scripts/teamviewer.sh
+++ config/scripts/teamviewer.sh
@@ -3,6 +3,6 @@ set -oue pipefail
 
 curl -L https://download.teamviewer.com/download/linux/teamviewer_amd64.tar.xz -o /tmp/teamviewer.rpm
 
-tar -xvf /tmp/teamviewer.rpm -C /tmp
+tar -xvf /tmp/teamviewer.rpm -C /usr/lib/
 
-/tmp/teamviewer/tv-setup install force
+ln -sf /usr/lib/teamviewer/tv_bin/script/teamviewer /usr/bin/teamviewer
