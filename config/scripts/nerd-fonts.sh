4cfcca0d2fcdd735a995f47b66f3b2b1d477fce8
nhubao <baoprokute1234@gmail.com>
2023-10-09 15:59:37 +0700

    Restore ownership font

--- config/scripts/nerd-fonts.sh
+++ config/scripts/nerd-fonts.sh
@@ -19,6 +19,8 @@ for font in "${font_names[@]}"; do
     url=$(echo "$RELEASE" | yq ".assets[] | select(.name == \"$font.tar.xz\") | .url")
     curl -L "$url" -H 'Accept: application/octet-stream' -o "/tmp/$font.tar.xz"
     tar -xvf "/tmp/$font.tar.xz" -C $BASE_DIR --one-top-level
+    restorecon -Rv "$BASE_DIR/$font"
+    chown -R root:root "$BASE_DIR/$font"
 done
 
 fc-cache -f
