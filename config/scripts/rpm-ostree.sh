f6d7a127803e6603cf90d87350ebf58330537fa4
nhubao <baoprokute1234@gmail.com>
2023-11-04 00:03:52 +0700

    Fix expressvpn

--- config/scripts/rpm-ostree.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-rm /etc/yum.repos.d/expressvpn.repo
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched mutter mutter-common
