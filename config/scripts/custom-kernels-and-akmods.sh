5345450a63ec80e420dc23e63e9c7aa2cfc1c0d6
nhubao <baoprokute1234@gmail.com>
2023-11-09 15:05:16 +0700

    Revert "Fix build"
    This reverts commit 485ecdfee87bf79ac97ae4358e61482e84b232a2.

--- config/scripts/custom-kernels-and-akmods.sh
+++ config/scripts/custom-kernels-and-akmods.sh
@@ -11,20 +11,8 @@ curl -sSLf "https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/
 # curl -sSLf "https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos-addons/repo/fedora-$((fedora_version - 1))/bieszczaders-kernel-cachyos-addons-fedora-$((fedora_version - 1)).repo" \
 #     -o /etc/yum.repos.d/bieszczaders-kernel-cachyos-addons.repo
 
-wget https://github.com/linux-surface/linux-surface/releases/download/silverblue-20201215-1/kernel-20201215-1.x86_64.rpm -O \
-    /tmp/surface-kernel.rpm
 rpm-ostree cliwrap install-to-root /
-rpm-ostree override replace /tmp/surface-kernel.rpm \
-    --remove kernel-core \
-    --remove kernel-modules \
-    --remove kernel-modules-core \
-    --remove kernel-modules-extra \
-    --install kernel-cachyos \
-    --install kernel-cachyos-core \
-    --install kernel-cachyos-devel \
-    --install kernel-cachyos-devel-matched \
-    --install kernel-cachyos-headers \
-    --install kernel-cachyos-modules
+rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos || true
 
 # rpm-ostree install libcap-ng-devel procps-ng-devel uksmd
 
