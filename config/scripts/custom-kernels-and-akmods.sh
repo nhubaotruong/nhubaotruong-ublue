776da37d3280bcca6ef17463138444b2b1845725
nhubao <baoprokute1234@gmail.com>
2023-11-09 14:07:06 +0700

    Fix rpm-ostree cliwrap

--- config/scripts/custom-kernels-and-akmods.sh
+++ config/scripts/custom-kernels-and-akmods.sh
@@ -11,6 +11,7 @@ curl -sSLf "https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/
 # curl -sSLf "https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos-addons/repo/fedora-$((fedora_version - 1))/bieszczaders-kernel-cachyos-addons-fedora-$((fedora_version - 1)).repo" \
 #     -o /etc/yum.repos.d/bieszczaders-kernel-cachyos-addons.repo
 
+rpm-ostree cliwrap install-to-root /
 rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos
 
 # rpm-ostree install libcap-ng-devel procps-ng-devel uksmd
