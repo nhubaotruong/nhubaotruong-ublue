5fe87bf3cbe8f64ac96aa68786d55a8f62eb3e1b
nhubao <baoprokute1234@gmail.com>
2023-11-10 17:24:45 +0700

    Fix

--- config/scripts/replace-vf-fonts.sh
+++ config/scripts/replace-vf-fonts.sh
@@ -8,9 +8,13 @@ mapfile -t font_names < <(rpm -qa | grep -oP "(.+)(?=-vf-fonts)")
 mapfile -t non_vf_fonts_names < <(printf '%s-fonts\n' "${font_names[@]}")
 
 # Remove not existing string fonts
-delete=(google-noto-sans-mono-cjk)
-for del in "${delete[@]}"; do
-    non_vf_fonts_names=("${non_vf_fonts_names[@]/$del-fonts/}")
+to_delete=(google-noto-sans-mono-cjk)
+for i in "${!non_vf_fonts_names[@]}"; do
+    for j in "${to_delete[@]}"; do
+        if [ "${non_vf_fonts_names[i]}" = "$j" ]; then
+            unset 'non_vf_fonts_names[i]'
+        fi
+    done
 done
 
 rpm-ostree install "${non_vf_fonts_names[@]}"
