ef7e5391e0b92563de5b53d342b3e9453a428fce
nhubao <baoprokute1234@gmail.com>
2023-11-24 16:59:45 +0700

    Fix build

--- config/scripts/remove-i686.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-i686_packages=$(rpm -qa | grep 'i686$' | tr '\n' ' ')
-
-rpm-ostree override remove $i686_packages
