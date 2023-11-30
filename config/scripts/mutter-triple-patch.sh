ab14bb50ac720fc876fb875c8e8ede426015046c
nhubao <baoprokute1234@gmail.com>
2023-11-30 15:22:33 +0700

    Update build

--- config/scripts/mutter-triple-patch.sh
+++ config/scripts/mutter-triple-patch.sh
@@ -6,4 +6,4 @@ set -oue pipefail
 # Mutter Triple Patch
 fedora_version="$(rpm -E %fedora)"
 curl -sSLf "https://copr.fedorainfracloud.org/coprs/trixieua/mutter-patched/repo/fedora-$fedora_version/trixieua-mutter-patched-fedora-$fedora_version.repo" -o /etc/yum.repos.d/trixieua-mutter-patched.repo
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched mutter mutter-common
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched mutter mutter-common gnome-shell xorg-x11-server-Xwayland
