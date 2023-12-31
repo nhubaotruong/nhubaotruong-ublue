dffd5c203ea2d6087e4480c97a046de942cf4c22
nhubao <baoprokute1234@gmail.com>
2023-12-31 21:19:32 +0700

    Use weston for waydroid

--- config/scripts/waydroid.sh
+++ config/scripts/waydroid.sh
@@ -5,6 +5,6 @@ sed -i~ -E 's/=.\$\(command -v (nft|ip6?tables-legacy).*/=/g' /usr/lib/waydroid/
 
 systemctl disable waydroid-container.service
 sed -i 's@Exec=waydroid first-launch@Exec=/usr/bin/waydroid-launcher first-launch\nX-Steam-Library-Capsule=/usr/share/applications/Waydroid/capsule.png\nX-Steam-Library-Hero=/usr/share/applications/Waydroid/hero.png\nX-Steam-Library-Logo=/usr/share/applications/Waydroid/logo.png\nX-Steam-Library-StoreCapsule=/usr/share/applications/Waydroid/store-logo.png\nX-Steam-Controller-Template=Desktop@g' /usr/share/applications/Waydroid.desktop
-# rm /usr/share/wayland-sessions/weston.desktop
+rm /usr/share/wayland-sessions/weston.desktop
 wget https://raw.githubusercontent.com/Quackdoc/waydroid-scripts/main/waydroid-choose-gpu.sh -O /usr/bin/waydroid-choose-gpu
 chmod +x /usr/bin/waydroid-choose-gpu
