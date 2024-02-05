f6ffb33c6f752acc848e288b4c2196bbd9d72ef5
nhubao <baoprokute1234@gmail.com>
2024-02-05 17:03:59 +0700

    Add kernel signing

--- config/scripts/microsoft-fonts.sh
+++ config/scripts/microsoft-fonts.sh
@@ -4,7 +4,7 @@
 set -oue pipefail
 
 # Microsoft Fonts
-curl -sSLf http://archlinuxgr.tiven.org/archlinux/x86_64/ttf-ms-win11-auto-10.0.22621.525-1-any.pkg.tar.zst | tar --use-compress-program=unzstd -x -C /
+curl -sSLf http://archlinuxgr.tiven.org/archlinux/x86_64/ttf-ms-win11-auto-10.0.22631.2428-1-any.pkg.tar.zst | tar --use-compress-program=unzstd -x -C /
 
 chown -R root: /usr/share/fonts/TTF
 chmod 644 /usr/share/fonts/TTF/*
