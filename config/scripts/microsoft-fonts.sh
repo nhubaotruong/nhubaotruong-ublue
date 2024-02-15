02665b014752c49d2b275b3549272d0b6ad203d9
nhubao <baoprokute1234@gmail.com>
2024-02-15 17:58:24 +0700

    Update microsoft fonts

--- config/scripts/microsoft-fonts.sh
+++ config/scripts/microsoft-fonts.sh
@@ -4,7 +4,7 @@
 set -oue pipefail
 
 # Microsoft Fonts
-curl -sSLf http://archlinuxgr.tiven.org/archlinux/x86_64/ttf-ms-win11-auto-10.0.22631.2428-1-any.pkg.tar.zst | tar --use-compress-program=unzstd -x -C /
+curl -sSLf https://dx37.gitlab.io/dx37essentials/x86_64/ttf-ms-win10-10.0.19043.1055-1-any.pkg.tar.zst | tar --use-compress-program=unzstd -x -C /
 
 chown -R root: /usr/share/fonts/TTF
 chmod 644 /usr/share/fonts/TTF/*
