9008fd501113c64ad5acb7bb75ddcda78874cb3f
nhubao <baoprokute1234@gmail.com>
2024-02-26 23:29:09 +0700

    Fix mdatp

--- config/scripts/mdatp.sh
+++ config/scripts/mdatp.sh
@@ -37,9 +37,9 @@ mkdir -p /usr/lib/systemd/system/mdatp.service.d
 cat <<EOF >/usr/lib/systemd/system/mdatp.service.d/override.conf
 [Service]
 ExecStartPre=/usr/bin/mount -t overlay overlay -o lowerdir=/usr/lib/microsoft,upperdir=/var/opt/microsoft,workdir=/var/microsoft-workdir /var/opt/microsoft
-ExecStartPre=/usr/sbin/semodule -i /var/opt/microsoft/mdatp/conf/selinux_policies/out/audisp_mdatp.pp
-ExecStartPre=/usr/sbin/semanage fcontext -a -e /opt/microsoft/mdatp /var/opt/microsoft/mdatp
-ExecStartPre=/usr/sbin/restorecon -vR /var/opt/microsoft/mdatp
+ExecStartPre=LD_LIBRARY_PATH="" /usr/sbin/semodule -i /var/opt/microsoft/mdatp/conf/selinux_policies/out/audisp_mdatp.pp
+ExecStartPre=LD_LIBRARY_PATH="" /usr/sbin/semanage fcontext -a -e /opt/microsoft/mdatp /var/opt/microsoft/mdatp
+ExecStartPre=LD_LIBRARY_PATH="" /usr/sbin/restorecon -vR /var/opt/microsoft/mdatp
 ExecStop=/usr/bin/umount /var/opt/microsoft
 EOF
 
