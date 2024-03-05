89a0a60591ac4cff2c5bdb29e18df99d728beb05
nhubao <baoprokute1234@gmail.com>
2024-03-05 12:10:34 +0700

    Fix selinux

--- config/scripts/selinux.sh
+++ config/scripts/selinux.sh
@@ -3,10 +3,6 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-find /usr/share/selinux -type f -name "*.pp*" | while read -r file; do
-    semodule -i "$file"
-done
-
 restorecon -vFr /
 
 cp -R /etc/selinux /usr/etc/selinux
