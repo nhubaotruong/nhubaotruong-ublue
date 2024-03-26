c95e38b6d71a41f89a0cc849bd304ec8fe0598c0
nhubao <baoprokute1234@gmail.com>
2024-03-26 14:25:41 +0700

    Fix install script

--- config/scripts/distrobox.sh
+++ config/scripts/distrobox.sh
@@ -3,4 +3,5 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
+mkdir -p /root
 curl -Ls https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --next --prefix /usr
