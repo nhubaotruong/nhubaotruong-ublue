5509b63ec0a922449c5ea86e7545057939d33d54
nhubao <baoprokute1234@gmail.com>
2024-03-26 13:24:49 +0700

    Install distrobox from github

---
+++ config/scripts/distrobox.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+curl -Ls https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix /usr
