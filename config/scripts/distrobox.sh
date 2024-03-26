942f42ec342e0d5959abfa119add8f943fd5d973
nhubao <baoprokute1234@gmail.com>
2024-03-26 14:36:14 +0700

    Revert install distrobox from script

--- config/scripts/distrobox.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-mkdir -p /root
-curl -Ls https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --next --prefix /usr
