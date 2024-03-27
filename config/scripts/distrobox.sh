7b1afdb1036abe9c09f0e9a99e5324c6a261b9da
nhubao <baoprokute1234@gmail.com>
2024-03-27 10:51:01 +0700

    Fix build

--- config/scripts/distrobox.sh
+++ config/scripts/distrobox.sh
@@ -3,4 +3,13 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-env HOME=/tmp curl -Ls https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix /usr
+alias cd='cd_func'
+
+cd_func() {
+    if [ $# -eq 0 ]; then
+        builtin cd /tmp
+    else
+        builtin cd "$@"
+    fi
+}
+curl -Ls https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix /usr
