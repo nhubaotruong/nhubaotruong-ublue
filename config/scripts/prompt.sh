ba1567b296708897a7acb259f2a91fd1e05c0f0d
nhubao <baoprokute1234@gmail.com>
2024-04-03 13:37:58 +0700

    Added ptysis terminal

--- config/scripts/prompt.sh
+++ config/scripts/prompt.sh
@@ -3,12 +3,10 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-fedora_version="$(rpm -E %fedora)"
-wget "https://copr.fedorainfracloud.org/coprs/kylegospo/prompt/repo/fedora-$fedora_version/kylegospo-prompt-fedora-$fedora_version.repo?arch=x86_64" -O /etc/yum.repos.d/_copr_kylegospo-prompt.repo
 rpm-ostree override replace \
     --experimental \
-    --from repo=copr:copr.fedorainfracloud.org:kylegospo:prompt \
+    --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
     vte291 \
-    vte-profile \
-    libadwaita
-rpm-ostree install ptyxis
+    vte-profile
+rpm-ostree install \
+    ptyxis
