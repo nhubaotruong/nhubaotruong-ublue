48480f87814dc80b5e5d988b508a7a67365ba3a3
nhubao <baoprokute1234@gmail.com>
2024-04-03 14:12:47 +0700

    Fix build

--- config/scripts/prompt.sh
+++ config/scripts/prompt.sh
@@ -6,7 +6,6 @@ set -oue pipefail
 rpm-ostree override replace \
     --experimental \
     --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
-    vte291 \
-    vte-profile
+    vte291
 rpm-ostree install \
     ptyxis
