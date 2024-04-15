2c73af5d2fe18e383d733f3ed04518e70ea1c161
nhubao <baoprokute1234@gmail.com>
2024-04-15 23:25:26 +0700

    Fix initramfs.sh

--- config/scripts/initramfs.sh
+++ config/scripts/initramfs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 set -oue pipefail
-
+KERNEL_SUFFIX=""
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
 /usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
