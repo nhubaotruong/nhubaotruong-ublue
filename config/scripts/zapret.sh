762661491e8da1b3aedf09be3d48f2ff4bfd3f4f
nhubao <baoprokute1234@gmail.com>
2024-05-09 16:56:45 +0700

    Add zapret

---
+++ config/scripts/zapret.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# set -oeux pipefail
+
+rpm-ostree install zlib-devel libcap-ng-devel libnetfilter_queue-devel
+
+git clone https://github.com/bol-van/zapret.git /tmp/zapret
+
+cd /tmp/zapret
+
+make -C /usr/lib/zapret
+
+ln -fs /usr/lib/zapret/zapret/init.d/systemd/zapret.service /usr/lib/zapret/systemd/system
+ln -fs /usr/lib/zapret/init.d/systemd/zapret-list-update.service /usr/lib/systemd/system
+ln -fs /usr/lib/zapret/init.d/systemd/zapret-list-update.timer /usr/lib/systemd/system
+
+/usr/lib/zapret/ipset/clear_lists.sh
+/usr/lib/zapret/ipset/get_config.sh
+
+systemctl enable zapret.serviec zapret-list-update.timer
