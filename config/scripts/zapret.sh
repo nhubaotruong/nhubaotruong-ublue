1415cf915638400581db8b4cee1a14662b6d241f
nhubao <baoprokute1234@gmail.com>
2024-05-09 17:15:40 +0700

    Fix build

--- config/scripts/zapret.sh
+++ config/scripts/zapret.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
-# set -oeux pipefail
+set -oeux pipefail
 
-rpm-ostree install zlib-devel libcap-ng-devel libnetfilter_queue-devel
+rpm-ostree install zlib-ng-devel libcap-ng-devel libnetfilter_queue-devel
 
-git clone https://github.com/bol-van/zapret.git /tmp/zapret
+mkdir -p /var/opt && cd /var/opt
+git clone https://github.com/bol-van/zapret.gits
 
-cd /tmp/zapret
+make -C /opt/zapret
 
-mkdir -p /usr/lib/zapret
+ln -fs /opt/zapret/zapret/init.d/systemd/zapret.service /usr/lib/zapret/systemd/system
+ln -fs /opt/zapret/init.d/systemd/zapret-list-update.service /usr/lib/systemd/system
+ln -fs /opt/zapret/init.d/systemd/zapret-list-update.timer /usr/lib/systemd/system
 
-make -C /usr/lib/zapret
-
-ln -fs /usr/lib/zapret/zapret/init.d/systemd/zapret.service /usr/lib/zapret/systemd/system
-ln -fs /usr/lib/zapret/init.d/systemd/zapret-list-update.service /usr/lib/systemd/system
-ln -fs /usr/lib/zapret/init.d/systemd/zapret-list-update.timer /usr/lib/systemd/system
-
-/usr/lib/zapret/ipset/clear_lists.sh
-/usr/lib/zapret/ipset/get_config.sh
+/opt/zapret/ipset/clear_lists.sh
+/opt/zapret/ipset/get_config.sh
 
 systemctl enable zapret.serviec zapret-list-update.timer
