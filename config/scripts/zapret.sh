95c473c9a42f5a6927dec5d73079399496d4b710
nhubao <baoprokute1234@gmail.com>
2024-05-09 17:48:07 +0700

    Fix zapret

--- config/scripts/zapret.sh
+++ config/scripts/zapret.sh
@@ -2,18 +2,23 @@
 
 set -oeux pipefail
 
-rpm-ostree install zlib-ng-devel libcap-ng-devel libnetfilter_queue-devel
-
 mkdir -p /var/opt && cd /var/opt
 git clone --depth 1 https://github.com/bol-van/zapret
 
 /opt/zapret/install_bin.sh
 
-ln -fs /opt/zapret/zapret/init.d/systemd/zapret.service /usr/lib/zapret/systemd/system
-ln -fs /opt/zapret/init.d/systemd/zapret-list-update.service /usr/lib/systemd/system
-ln -fs /opt/zapret/init.d/systemd/zapret-list-update.timer /usr/lib/systemd/system
+ln -fs /opt/zapret/init.d/systemd/* /usr/lib/systemd/system
 
 /opt/zapret/ipset/clear_lists.sh
 /opt/zapret/ipset/get_config.sh
 
 systemctl enable zapret.serviec zapret-list-update.timer
+
+mv /var/opt/zapret /usr/lib/zapret
+
+cat <<EOF >/usr/lib/tmpfiles.d/teamviewer.conf
+L  /opt/zapret  -  -  -  -  /usr/lib/zapret
+EOF
+cat <<EOF >/usr/lib/sysusers.d/zapret.conf
+u zapret - "zapret dpi bypasser"
+EOF
