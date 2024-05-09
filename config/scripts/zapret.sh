0f6763c91f9285b894cedafc477492364df8b82f
nhubao <baoprokute1234@gmail.com>
2024-05-09 17:28:17 +0700

    Fix zapret

--- config/scripts/zapret.sh
+++ config/scripts/zapret.sh
@@ -5,9 +5,9 @@ set -oeux pipefail
 rpm-ostree install zlib-ng-devel libcap-ng-devel libnetfilter_queue-devel
 
 mkdir -p /var/opt && cd /var/opt
-git clone https://github.com/bol-van/zapret.gits
+git clone --depth 1 https://github.com/bol-van/zapret
 
-make -C /opt/zapret
+/opt/zapret/install_bin.sh
 
 ln -fs /opt/zapret/zapret/init.d/systemd/zapret.service /usr/lib/zapret/systemd/system
 ln -fs /opt/zapret/init.d/systemd/zapret-list-update.service /usr/lib/systemd/system
