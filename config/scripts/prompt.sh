19e2e634dd5863c99e21b0bd441b3d423c295c73
nhubao <baoprokute1234@gmail.com>
2024-05-10 16:12:39 +0700

    Fix build

--- config/scripts/prompt.sh
+++ config/scripts/prompt.sh
@@ -3,6 +3,9 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
+fedora_version=$(rpm -E %fedora)
+
+wget "https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$fedora_version/ublue-os-staging-fedora-$fedora_version.repo" -O "/etc/yum.repos.d/ublue-os-staging-fedora-$fedora_version.repo"
 rpm-ostree override replace \
     --experimental \
     --freeze \
