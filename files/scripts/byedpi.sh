0345305ca8297a91e8043d9441e801881bf153a1
nhubaotruong <baoprokute1234@gmail.com>
2024-10-21 15:53:21 +0700

    byedpi use latest tag, not master branch

--- files/scripts/byedpi.sh
+++ files/scripts/byedpi.sh
@@ -7,6 +7,15 @@ git clone https://github.com/hufrea/byedpi.git /tmp/byedpi
 
 cd /tmp/byedpi
 
+# Fetch the latest tags
+git fetch --tags
+
+# Get the latest tag
+latest_tag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+
+# Checkout the latest tag
+git checkout "$latest_tag"
+
 make
 
 make PREFIX=/usr install
