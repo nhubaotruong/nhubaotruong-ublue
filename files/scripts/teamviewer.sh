40095b2515d11548cb84b4d391e2310d3d9d450c
nhubaotruong <baoprokute1234@gmail.com>
2024-11-16 14:18:02 +0700

    Fix teamviewer.sh install script

--- files/scripts/teamviewer.sh
+++ files/scripts/teamviewer.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-# Setup repo
-cat <<EOF >/etc/yum.repos.d/teamviewer.repo
-[teamviewer]
-name=TeamViewer - $basearch
-baseurl=https://linux.teamviewer.com/yum/stable/main/binary-$basearch/
-gpgkey=https://linux.teamviewer.com/pubkey/currentkey.asc
-gpgcheck=1
-repo_gpgcheck=1
-enabled=1
-type=rpm-md
-EOF
-
-rpm --import https://linux.teamviewer.com/pubkey/currentkey.asc
-
-rpm-ostree install teamviewer
+rpm-ostree install https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
 
 rm -f /etc/yum.repos.d/teamviewer.repo
