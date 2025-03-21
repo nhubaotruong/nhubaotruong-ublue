24ac4781f3e356a86514ba48c58ab53caeeca2d2
nhubaotruong <baoprokute1234@gmail.com>
2025-03-21 11:58:27 +0700

    Update teamviewer script

--- files/scripts/teamviewer.sh
+++ files/scripts/teamviewer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-rpm-ostree install https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
+dnf5 -y install https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
 
 rm -f /etc/yum.repos.d/teamviewer.repo
