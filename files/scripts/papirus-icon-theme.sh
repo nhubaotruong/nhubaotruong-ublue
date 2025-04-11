6b6cf05345b130a77a2cfcbbe4a77d5b84d22a1a
nhubaotruong <baoprokute1234@gmail.com>
2025-04-11 14:17:42 +0700

    Fix papirus symlink

--- files/scripts/papirus-icon-theme.sh
+++ files/scripts/papirus-icon-theme.sh
@@ -4,4 +4,5 @@
 set -oue pipefail
 export EXTRA_THEMES="Papirus-Dark"
 wget -qO- https://git.io/papirus-icon-theme-install | sh
-fc-cache -rs
+ln -sf /usr/share/icons/Papirus/16x16/symbolic /usr/share/icons/Papirus/
+ln -sf /usr/share/icons/Papirus-Dark/16x16/symbolic /usr/share/icons/Papirus-Dark/
