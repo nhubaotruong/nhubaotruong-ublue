7738a35f907751a079a9a39217bd613c5cc8d059
nhubaotruong <baoprokute1234@gmail.com>
2025-04-22 11:24:49 +0700

    Update build

--- files/scripts/mdatp.sh
+++ files/scripts/mdatp.sh
@@ -11,7 +11,7 @@ set -oue pipefail
 mkdir -p /var/opt
 
 # Setup repo
-curl -sSLf "https://packages.microsoft.com/config/rhel/8/prod.repo" -o /etc/yum.repos.d/microsoft.repo
+curl -sSLf "https://packages.microsoft.com/config/rhel/8/insiders-slow.repo" -o /etc/yum.repos.d/microsoft.repo
 
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
