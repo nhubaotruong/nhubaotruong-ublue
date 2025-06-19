dc5a18b8eee59a0d3946bdfc50b1612b04b97a02
nhubaotruong <baoprokute1234@gmail.com>
2025-06-19 12:11:58 +0700

    Fix msfont

--- files/scripts/microsoft-fonts.sh
+++ files/scripts/microsoft-fonts.sh
@@ -4,7 +4,7 @@
 set -oue pipefail
 
 # Microsoft Fonts
-curl -sSLf https://dx37.gitlab.io/dx37essentials/x86_64/ttf-ms-win10-10.0.19043.1055-1-any.pkg.tar.zst | tar --use-compress-program=unzstd -x -C /
+curl -sSLf https://archlinuxgr.tiven.org/archlinux/x86_64/ttf-ms-win11-auto-10.0.26100.1742-4-any.pkg.tar.zst | tar --use-compress-program=unzstd -x -C /
 
 chown -R root: /usr/share/fonts/TTF
 chmod 644 /usr/share/fonts/TTF/*
