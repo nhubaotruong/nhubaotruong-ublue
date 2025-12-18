4390ec680d89463e34db4c82cf9463188bdf8533
nhubaotruong <baoprokute1234@gmail.com>
2025-12-18 17:51:48 +0700

    Revert "Fix bazzite.sh"
    This reverts commit e0c09350a1975682b4d6cfdf216dda1ccdf48a3a.

--- files/scripts/bazzite.sh
+++ files/scripts/bazzite.sh
@@ -2,9 +2,9 @@
 set -oue pipefail
 
 sed -i 's|#default.clock.allowed-rates = \[ 48000 \]|default.clock.allowed-rates = [ 44100 48000 ]|' /usr/share/pipewire/pipewire.conf
-# sed -i 's/balanced=balanced$/balanced=balanced-bazzite/' /etc/tuned/ppd.conf
-# sed -i 's/performance=throughput-performance$/performance=throughput-performance-bazzite/' /etc/tuned/ppd.conf
-# sed -i 's/balanced=balanced-battery$/balanced=balanced-battery-bazzite/' /etc/tuned/ppd.conf
+sed -i 's/balanced=balanced$/balanced=balanced-bazzite/' /etc/tuned/ppd.conf
+sed -i 's/performance=throughput-performance$/performance=throughput-performance-bazzite/' /etc/tuned/ppd.conf
+sed -i 's/balanced=balanced-battery$/balanced=balanced-battery-bazzite/' /etc/tuned/ppd.conf
 
 # cp /usr/share/auto-cpufreq/scripts/cpufreqctl.sh /usr/bin/cpufreqctl.auto-cpufreq
 # chmod +x /usr/bin/cpufreqctl.auto-cpufreq
