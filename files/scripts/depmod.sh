f56adf3beddb0c79efa14a8b43dee99f3d1bee6a
nhubaotruong <baoprokute1234@gmail.com>
2026-01-26 17:51:16 +0700

    Fix depmod.sh

--- files/scripts/depmod.sh
+++ files/scripts/depmod.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-set -oue pipefail
 
-depmod -a -v "$(ls /usr/lib/modules)"
+for module_dir in /usr/lib/modules/*; do
+    if [ -d "$module_dir" ]; then
+        echo "Running depmod on $module_dir"
+        depmod -a -v "$(basename "$module_dir")" 2>/dev/null || echo "Ignored error for $module_dir"
+    fi
+done
