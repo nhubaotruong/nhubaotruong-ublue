1cbafd64ce71d4dfab99af07afac271c4a71ba06
nhubaotruong <baoprokute1234@gmail.com>
2026-03-29 10:57:41 +0700

    Update setup swap

---
+++ files/files/usr/libexec/setup-swap-ext4.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/sh
+set -e
+
+cd "$(dirname "$0")"
+
+size=12G
+
+if [ "$(whoami)" != "root" ]; then
+    echo "Script must be run as root" >&2
+    exit 1
+fi
+
+if [ "$1" == "--recreate" ]; then
+    shift
+    swapoff /var/swap/swapfile || true
+    rm /var/swap/swapfile
+elif [ -e /var/swap/swapfile ]; then
+    echo "/var/swap/swapfile already exists, use --recreate to recreate" >&2
+    exit 1
+fi
+
+[ -n "$1" ] && size="$1"
+
+free_bytes="$(($(stat -f --format="%a*%S" .)))"
+size_bytes="$(numfmt --from=auto "$size")"
+
+[ ! -e /var/swap ] && mkdir -p /var/swap
+fallocate -l "$size" /var/swap/swapfile
+chmod 0600 /var/swap/swapfile
+
+semanage fcontext -a -t var_t -f d '/var/swap' || true
+semanage fcontext -a -t swapfile_t -f f '/var/swap/swapfile' || true
+restorecon -v -r '/var/swap'
+
+mkswap /var/swap/swapfile
+swapon /var/swap/swapfile
+grep -q /var/swap/swapfile /etc/fstab || echo '/var/swap/swapfile swap swap sw 0 0' >> /etc/fstab
+
+# If we got this far, disable it for good measure (even though it should not run again)
+systemctl disable setup-swap-ext4.service
